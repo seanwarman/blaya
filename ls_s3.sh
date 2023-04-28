d0098c1cf30b6efe808e61453e1a66f1fbc10339
Ubuntu <ubuntu@ip-172-31-32-80.eu-west-2.compute.internal>
2023-04-28 05:08:42 +0000

    New mover script and update to aws track list script

--- ls_s3.sh
+++ ls_s3.sh
@@ -1,3 +1,3 @@
 echo 'export const trackList = [' > $1
-aws s3 ls everest-files --recursive | awk 'BEGIN {FS=" "} {$1=$2=$3=null;$url=substr($0,4);print "\x09\x22" $url "\x22,"}' >> $1
+aws s3 ls --recursive everest-files/music/ | awk '/.mp3/ {print}' | awk 'BEGIN {FS=" "} {$1=$2=$3=null;$url=substr($0,4);print "\x09\x22" $url "\x22,"}' >> $1
 echo ']' >> $1
--- mover.mjs
+++ mover.mjs
@@ -1,15 +1,14 @@
 import { spawn } from 'child_process'
-import { dirlist } from './public/dir-list.js'
 import { parseFile } from 'music-metadata'
-import { readdir } from 'node:fs/promises';
+import { tracklist } from './track-list.js'
 
 const bucket = 's3://everest-files/'
 const local = 'workspace/'
 
 function copy(i = 0) {
-  const dir = dirlist[i]
-  console.log(`@FILTER dir:`, dir)
-  const cp = spawn('aws', ['s3', 'cp', '--recursive', bucket + 'complete/' + dir, local + dir])
+  const file = tracklist[i]
+  console.log(`@FILTER file:`, file)
+  const cp = spawn('aws', ['s3', 'cp', bucket + file, local + file])
 
   cp.stdout.on('data', d => {
     console.log(`@FILTER d.toString():`, d.toString())
@@ -20,80 +19,56 @@ function copy(i = 0) {
   })
 
   cp.on('close', () => {
-    console.log(`@FILTER copied: `, dir)
-    readDir(i)
+    console.log(`@FILTER copied: `, file)
+    parser(i)
   })
-}
-
-async function readDir(i) {
-  const dir = dirlist[i]
-  console.log(`@FILTER dir:`, dir)
-
-  try {
-    const files = await readdir(local + dir)
-    if (!files.length) {
-      console.log(`@FILTER no files for dir: `, dir)
-      // next dir
-      remove(() => copy(i + 1))
-      return
-    }
-
-    mover(i, 0, files)
-
-  } catch (e) {
-    console.log(`@FILTER readDir error:`, e)
-    // next dir
-    // copy(i + 1)
-  }
 }
 
 function remove(then) {
-  const rm = spawn('rm', ['-rf', 'workspace'])
-  rm.on('close', () => {
-    const mkdir = spawn('mkdir', ['workspace'])
-    mkdir.on('close', then)
-  })
+  then()
+  // const rm = spawn('rm', ['-rf', 'workspace'])
+  // rm.on('close', () => {
+  //   const mkdir = spawn('mkdir', ['workspace'])
+  //   mkdir.on('close', then)
+  // })
 }
 
-async function mover(dirI, i = 0, files) {
-  const dir = dirlist[dirI]
-  const file = files[i]
-  try {
-    const { common } = await parseFile(local + dir + file)
-    const { title, year, album, artist } = common
+function newPath({ artist, album, year, title, file }) {
+  if (artist && album && year && title) return bucket + 'music/' + artist + ' - ' + album + ' - ' + year + '/' + title + '.mp3'
+  if (artist && album && title) return bucket + 'music/' + artist + ' - ' + album + '/' + title + '.mp3'
+  if (artist && title) return bucket + 'music/' + artist +  '/' + title + '.mp3'
+  if (artist) return bucket + 'music/' + artist +  '/' + file.slice(8)
+  return bucket + 'music/' + file.slice(8)
+}
 
-    const mv = spawn('aws', ['s3', 'mv', bucket + 'complete/' + dir + file,
-      bucket + 'music/' + artist + ' - ' + album + ' - ' + year + '/' + title + '.mp3'
+async function parser(i) {
+  const file = tracklist[i]
+  try {
+    const { common } = await parseFile(local + file)
+    const { title, year, album, artist } = common
+    console.log(`title:`, title)
+    console.log(`year:`, year)
+    console.log(`album:`, album)
+    console.log(`artist:`, artist)
+
+    const mv = spawn('aws', ['s3', 'mv', bucket + file,
+      newPath({ title, album, artist, year, file })
     ])
 
     mv.on('error', e => {
-      console.log(`@FILTER mv error:`, e)
+      console.log(`mv error:`, e)
     })
     mv.on('close', () => {
-      console.log(`@FILTER file:`, file)
-      console.log(`@FILTER title:`, title)
-      if (files[i + 1]) {
-        mover(dirI, i + 1, files)
-      } else {
-        // next dir
-        copy(dirI + 1)
-      }
+      console.log(`Copy completed!`)
+      console.log(`file:`, file)
+      console.log(`title:`, title)
+
+      remove(() => copy(i + 1))
     })
+
   } catch (error) {
-    console.log(`@FILTER parseFile error:`, error)
-    // next dir
-    // copy(dirI + 1)
-    if (files[i + 1]) {
-      console.log(`@FILTER next file...`)
-      mover(dirI, i + 1, files)
-    } else if (dirlist[dirI + 1]) {
-      console.log(`@FILTER next dir`)
-      // next dir
-      remove(() => copy(dirI + 1))
-    } else {
-      console.log(`@FILTER can't move on (shrug)`)
-      console.log(`@FILTER dirI:`, dirI)
-    }
+    console.log(`Parsing error: `, error)
+    copy(i + 1)
   }
 }
 
