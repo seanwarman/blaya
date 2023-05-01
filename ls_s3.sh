de607f8f6066a2812e86da8f3ef67472334b614c
SeanShine <s.warman@myshine.co.uk>
2023-05-01 05:47:12 +0100

    Ls script to escape double quotes

--- ls_s3.sh
+++ ls_s3.sh
@@ -1,3 +1,3 @@
 echo 'export const trackList = [' > $1
-aws s3 ls --recursive everest-files/music/ | awk '/.mp3/ {print}' | awk 'BEGIN {FS=" "} {$1=$2=$3=null;$url=substr($0,4);print "\x09\x22" $url "\x22,"}' >> $1
+aws s3 ls --recursive everest-files/music/ | awk '/.mp3/ {print}' | awk 'BEGIN {FS=" "} {$1=$2=$3=null; $url=substr($0,4); gsub(/"/, "\\\x22", $url); print "\x09\x22" $url "\x22,"}' >> $1
 echo ']' >> $1
