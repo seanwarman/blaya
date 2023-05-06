ca5ccad42d8b97eb2d09e9985e4bb22d01277e9d
Ubuntu <ubuntu@ip-172-31-32-80.eu-west-2.compute.internal>
2023-05-06 15:46:39 +0000

    Update ls script to run from anywhere

--- ls_s3.sh
+++ ls_s3.sh
@@ -1,3 +1,4 @@
-echo 'export const trackList = [' > $1
-aws s3 ls --recursive everest-files/music/ | awk '/.mp3/ {print}' | awk 'BEGIN {FS=" "} {$1=$2=$3=null; $url=substr($0,4); gsub(/"/, "\\\x22", $url); print "\x09\x22" $url "\x22,"}' >> $1
-echo ']' >> $1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo 'export const trackList = [' > $SCRIPT_DIR/public/track-list.js
+aws s3 ls --recursive everest-files/music/ | awk '/.mp3/ {print}' | awk 'BEGIN {FS=" "} {$1=$2=$3=null; $url=substr($0,4); gsub(/"/, "\\\x22", $url); print "\x09\x22" $url "\x22,"}' >> $SCRIPT_DIR/public/track-list.js
+echo ']' >> $SCRIPT_DIR/public/track-list.js
