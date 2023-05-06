120522f41ba6bdcf825808c7a6559fbd7e9e3f14
Ubuntu <ubuntu@ip-172-31-32-80.eu-west-2.compute.internal>
2023-05-06 15:48:22 +0000

    Upload controller to refresh tracks

--- router.js
+++ router.js
@@ -1,17 +1,31 @@
+import { spawn } from 'child_process'
 import multer from 'multer'
 import { cleanDir, copySendFile } from './controllers/trackList.js'
 import { gitPullOrigin } from './controllers/update.js'
 import { copy } from './mover.mjs'
-const upload = multer({ dest: 'workspace/' })
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'workspace/'),
+    filename: (req, file, cb) => cb(null, file.originalname),
+  })
+})
 
 export default app => {
   app.get('/api/update', gitPullOrigin)
   app.get('/music/*', cleanDir, copySendFile)
   app.post('/upload', cleanDir, upload.array('files'), (req, res) => {
-    const { body, files } = req
+    const { files } = req
     console.log(`@FILTER files:`, files)
     console.log(`@FILTER copy:`, copy)
-    // copy(0, files.map(({ filename }) => filename))
-    res.send('Upload ok')
+    copy(0, files.map(({ originalname }) => originalname), () => {
+      console.log('Refreshing track list...')
+
+      const script = spawn('./ls_s3.sh')
+
+      script.on('close', () => {
+        console.log(`@FILTER Tracks refreshed`)
+        res.send('Uploaded')
+      })
+    })
   })
 }
