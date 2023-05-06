03705a96b432d05148ef4540979c0573723bf9a2
Ubuntu <ubuntu@ip-172-31-32-80.eu-west-2.compute.internal>
2023-05-06 15:47:06 +0000

    Updated mover to copy

--- mover.mjs
+++ mover.mjs
@@ -4,8 +4,9 @@ import { parseFile } from 'music-metadata'
 const bucket = 's3://everest-files/'
 const local = 'workspace/'
 
-export function copy(i = 0, tracklist) {
+export function copy(i = 0, tracklist, then) {
   const file = tracklist[i]
+  if (!file) return then(tracklist)
   console.log(`@FILTER file:`, file)
   const cp = spawn('aws', ['s3', 'cp', bucket + file, local + file])
 
@@ -19,7 +20,7 @@ export function copy(i = 0, tracklist) {
 
   cp.on('close', () => {
     console.log(`@FILTER copied: `, file)
-    parser(i, tracklist)
+    parser(i, tracklist, then)
   })
 }
 
@@ -41,11 +42,11 @@ function newPath({ track, artist, album, year, title, file }) {
   if (artist && album && year && trackNoTitle) return bucket + 'music/' + artist + ' - ' + album + ' - ' + year + '/' + trackNoTitle + '.mp3'
   if (artist && album && trackNoTitle) return bucket + 'music/' + artist + ' - ' + album + '/' + trackNoTitle + '.mp3'
   if (artist && trackNoTitle) return bucket + 'music/' + artist +  '/' + trackNoTitle + '.mp3'
-  if (artist) return bucket + 'music/' + artist +  '/' + file.slice(8)
-  return bucket + 'music/' + file.slice(8)
+  if (artist) return bucket + 'music/' + artist +  '/' + file
+  return bucket + 'music/' + file
 }
 
-async function parser(i, tracklist) {
+async function parser(i, tracklist, then) {
   const file = tracklist[i]
   try {
     const { common } = await parseFile(local + file)
@@ -56,24 +57,24 @@ async function parser(i, tracklist) {
     console.log(`album:`, album)
     console.log(`artist:`, artist)
 
-    const mv = spawn('aws', ['s3', 'mv', bucket + file,
+    const cp = spawn('aws', ['s3', 'cp', local + file,
       newPath({ track, album, artist, year, file })
     ])
 
-    mv.on('error', e => {
-      console.log(`mv error:`, e)
+    cp.on('error', e => {
+      console.log(`cp error:`, e)
     })
-    mv.on('close', () => {
+    cp.on('close', () => {
       console.log(`Copy completed!`)
       console.log(`file:`, file)
       console.log(`title:`, title)
 
-      remove(() => copy(i + 1, tracklist))
+      remove(() => copy(i + 1, tracklist, then))
     })
 
   } catch (error) {
     console.log(`Parsing error: `, error)
-    copy(i + 1, tracklist)
+    copy(i + 1, tracklist, then)
   }
 }
 
