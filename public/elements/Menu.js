8c755069d62e1e5068cf8c6ba52972a563324984
SeanShine <s.warman@myshine.co.uk>
2023-07-29 21:18:01 +0100

    Created menu element "component"

---
+++ public/elements/Menu.js
@@ -0,0 +1,96 @@
+import * as ev from '../helpers/events.js'
+import * as h from '../helpers/index.js'
+import * as dom from '../helpers/dom.js'
+
+export default function Menu() {
+  return dom.div({
+    id: 'menu-container',
+    children: [
+      dom.div({
+        id: 'menu',
+        children: [
+          dom.div({
+            className: 'menu-activate',
+            innerText: '●●●',
+            onclick: ev.onClickOrEnter(ev.onOpenMenu),
+          }),
+          dom.ul({
+            className: 'menu-items closed',
+            children: [
+              Add(),
+              // TODO...
+              // Edit(),
+              Remove(),
+            ],
+          })
+        ],
+      })
+    ],
+  }) 
+}
+
+function Add() {
+  return dom.li({
+    id: 'tracklist-add-to-playlist-menu-item',
+    innerText: 'Add',
+    onclick: ev.onClickOrEnter(() => {
+      const els = document.getElementsByClassName('track-selected')
+      let playlists = window.state.playlists
+      for (const el of els) {
+        const { href } = el.parentElement?.dataset
+        if (!href) {
+          continue
+        }
+        playlists = h.addHrefToPlaylist(
+          window.state.selectedPlaylist, href, playlists
+        )
+      }
+      window.state.playlists = playlists
+      dom.flashPlaylist()
+      const ul = document.getElementsByClassName('menu-items')?.[0]
+      setTimeout(() => {
+        ul.classList.add('closed')
+      }, 200)
+    })
+  })
+}
+
+function Edit() {
+  return dom.li({
+    id: 'edit-track-menu-item',
+    innerText: 'Edit',
+    onclick: ev.onClickOrEnter(() => {
+      const els = document.getElementsByClassName('track-selected')
+      const hrefs = Array.from(els).map(el => el.parentElement?.dataset?.href)
+      console.log(`@FILTER hrefs:`, hrefs)
+    }),
+  })
+}
+
+function Remove() {
+  return dom.li({
+    id: 'playlist-remove-from-playlist-menu-item',
+    innerText: 'Remove',
+    onclick: ev.onClickOrEnter(() => {
+      const els = document.getElementsByClassName('track-selected')
+      let playlists = window.state.playlists
+      for (const el of els) {
+        if (!el.parentElement) {
+          continue
+        }
+        const i = h
+          .findIndexOfElement(
+            el.parentElement
+          )(document.getElementById('playlist').getElementsByClassName('track'))
+        playlists = h.removeTrackFromPlaylist(
+          window.state.selectedPlaylist, i, playlists
+        )
+      }
+      window.state.playlists = playlists
+      const ul = document.getElementsByClassName('menu-items')?.[0]
+      setTimeout(() => {
+        ul.classList.add('closed')
+      }, 200)
+    }),
+  })
+}
--- public/helpers/observers.js
+++ public/helpers/observers.js
@@ -1,7 +1,4 @@
-import * as f from './functional-utils.js'
-import * as ev from './events.js'
-import * as h from './index.js'
-import * as dom from './dom.js'
+import Menu from '../elements/Menu.js'
 
 export function observeTrackSelectedMenu(mutations) {
   const mutation = mutations[mutations.length-1]
@@ -10,79 +7,11 @@ export function observeTrackSelectedMenu(mutations) {
     && mutation.target.classList.contains('track-selected')
   ) {
     document.getElementById('menu-container')?.remove()
-    const ul = dom.ul({
-      className: 'menu-items closed',
-      children: [
-        dom.li({
-          id: 'tracklist-add-to-playlist-menu-item',
-          innerText: 'Add',
-          onclick: ev.onClickOrEnter(() => {
-            const els = document.getElementsByClassName('track-selected')
-            let playlists = window.state.playlists
-            for (const el of els) {
-              const { href } = el.parentElement?.dataset
-              if (!href) {
-                continue
-              }
-              playlists = h.addHrefToPlaylist(
-                window.state.selectedPlaylist, href, playlists
-              )
-            }
-            window.state.playlists = playlists
-            dom.flashPlaylist()
-            setTimeout(() => {
-              ul.classList.add('closed')
-            }, 200)
-          })
-        }),
-        dom.li({
-          id: 'playlist-remove-from-playlist-menu-item',
-          innerText: 'Remove',
-          onclick: ev.onClickOrEnter(() => {
-            const els = document.getElementsByClassName('track-selected')
-            let playlists = window.state.playlists
-            for (const el of els) {
-              if (!el.parentElement) {
-                continue
-              }
-              const i = h
-                .findIndexOfElement(
-                  el.parentElement
-                )(document.getElementById('playlist').getElementsByClassName('track'))
-              playlists = h.removeTrackFromPlaylist(
-                window.state.selectedPlaylist, i, playlists
-              )
-            }
-            window.state.playlists = playlists
-            setTimeout(() => {
-              ul.classList.add('closed')
-            }, 200)
-          }),
-        }),
-      ],
-    })
-    const menuEl = dom.div({
-      id: 'menu-container',
-      children: [
-        dom.div({
-          id: 'menu',
-          children: [
-            dom.div({
-              className: 'menu-activate',
-              innerText: '●●●',
-              onclick: ev.onClickOrEnter(ev.onOpenMenu),
-            }),
-            ul,
-          ],
-        })
-      ],
-    })
-
     mutation.target
       .parentElement
       .parentElement
       .insertBefore(
-        menuEl,
+        Menu(),
         mutation.target.parentElement.nextElementSibling
       )
   }
