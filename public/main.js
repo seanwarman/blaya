aa4063ec7170eaa8c88a6b33c818eb71b34887c0
SeanShine <s.warman@myshine.co.uk>
2023-08-26 19:49:41 +0100

    Fixed menu visibility bug

--- public/main.js
+++ public/main.js
@@ -123,6 +123,7 @@ build(state => {
   const onTogglePlaylistMinimised = () => {
     const playlistContainer = document.getElementById('playlist-container')
     playlistContainer.dataset.playlistMinimised = playlistContainer.dataset.playlistMinimised === 'false'
+    document.body.dataset.playlistMinimised = playlistContainer.dataset.playlistMinimised === 'true'
     document.getElementById('maximise-button-playlist').innerHTML = playlistContainer.dataset.playlistMinimised === 'false'
       ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>'
       : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>'
