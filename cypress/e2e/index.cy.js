2e2d23f2ae0a3de32f97de342cf40e62da397a25
Sean Warman <seanwarman@gmail.com>
2023-12-29 22:11:51 +0000

    Moved cypress into dev deps, removed test

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -3,11 +3,11 @@ describe('template spec', () => {
     cy.visit('/');
     cy.get('html').should('exist');
     cy.get('.mode-button-playlist.button-circle').should('exist').click({ force: true });
-    cy.get('.add-to-playlist').should('exist').then(([el]) => {
-      cy.wrap(el).click({ force: true });
-      cy.get('#playlist-container .track').should('have.lengthOf', 1);
-      cy.get('.remove-from-playlist-icon').click({ force: true });
-      cy.get('#playlist-container .track').should('have.lengthOf', 0);
-    });
+    // cy.get('.add-to-playlist').should('exist').then(([el]) => {
+    //   cy.wrap(el).click({ force: true });
+    //   cy.get('#playlist-container .track').should('have.lengthOf', 1);
+    //   cy.get('.remove-from-playlist-icon').click({ force: true });
+    //   cy.get('#playlist-container .track').should('have.lengthOf', 0);
+    // });
   });
 });
