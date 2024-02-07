7fff1e793868716cbe0ffd2f22aacf56265b9f9a
seanwarman <seanwarman@gmail.com>
2024-02-07 22:21:49 +0000

    Put requested audio buffer into data array bit by bit

--- public/test.js
+++ public/test.js
@@ -1,40 +1,42 @@
-const [start, stop] = document.querySelectorAll('button')
-start.disabled = true
-start.addEventListener('click', () => {
-  clock()
-  start.disabled = true
-})
-function createAudio(mapBuffer) {
+function createAudioBuffer(mapBuffer) {
   const audioCtx = new AudioContext();
-  const myArrayBuffer = audioCtx.createBuffer(
+  const audioBuffer = audioCtx.createBuffer(
     2,
     audioCtx.sampleRate * 3,
     audioCtx.sampleRate,
   );
-  for (let channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-    const nowBuffering = myArrayBuffer.getChannelData(channel);
-    for (let i = 0; i < myArrayBuffer.length; i++) {
+  for (let channel = 0; channel < audioBuffer.numberOfChannels; channel++) {
+    const nowBuffering = audioBuffer.getChannelData(channel);
+    for (let i = 0; i < audioBuffer.length; i++) {
       nowBuffering[i] = mapBuffer(i)
     }
   }
   let source;
   return function playSound(cueTime, startTime, endTime) {
     source = audioCtx.createBufferSource();
-    source.buffer = myArrayBuffer;
+    source.buffer = audioBuffer;
     source.connect(audioCtx.destination);
     source.start(cueTime, startTime, endTime);
     return source
   }
 }
 
-const eventLoop = createAudio(() => 0);
-const click = createAudio(i => {
+const eventLoop = createAudioBuffer(() => 0);
+const click = createAudioBuffer(i => {
   if (i < 100) {
     return Math.random() * 2 - 1
   }
   return 0
 })
 
+// TEST CODE --------- 
+const audioBufferPromis = fetch('Black-Mountain.mp3')
+  .then(res => res.arrayBuffer())
+  .then(buffer => {
+    const context = new AudioContext()
+    return context.decodeAudioData(buffer)
+  })
+// TEST CODE --------- 
 
 let source;
 let count = 0
@@ -42,6 +44,25 @@ const BPM = 120
 const STEP_RESOLUTION = 128
 const BPS = 60 / BPM
 const clockTime = BPS / STEP_RESOLUTION
+const [start, stop] = document.querySelectorAll('button')
+start.disabled = true
+start.addEventListener('click', () => {
+
+  // TEST CODE --------
+  audioBufferPromis.then(blackMountainAudioBuffer => {
+    const b = blackMountainAudioBuffer.getChannelData(1)
+    const playBlackMountain = createAudioBuffer((i) => {
+      return b[i]
+    })
+    playBlackMountain(0)
+  })
+  // TEST CODE --------
+
+  // Put this back...
+  // clock()
+  // start.disabled = true
+})
+
 start.disabled = false
 
 function clock() {
