bb43a7fab2d8d5a734f493e3c7994f9cea7941ed
seanwarman <seanwarman@gmail.com>
2024-12-27 21:26:10 +0000

    localStorage subscription and styling

--- public/v2/stores/playlist.js
+++ public/v2/stores/playlist.js
@@ -1,25 +1,21 @@
 import { defineStore } from 'pinia';
 
-const defaultPlaylists = [
+const INITIAL_PLAYLISTS_STATE = [
   {
     name: '',
     index: 0,
-    tracks: ["music/01 - Chris Avantgarde, Anyma (ofc) - Eternity (Extended Mix).mp3","music/01 Everything I own by Ken Boothe 1974 Trojan TRLS95 DMk1.mp3"],
+    tracks: [],
   },
 ];
+
+export const PLAYLISTS_STATE_KEY = 'blaya__PLAYLISTS_STATE_KEY_V2';
 
 export const usePlaylistStore = defineStore('playlist', {
   state: () => ({
     playlistsVisible: true,
     playlistMode: false,
     currentPlaylist: 0,
-    playlists: [
-      {
-        name: '',
-        index: 0,
-        tracks: ["music/01 - Chris Avantgarde, Anyma (ofc) - Eternity (Extended Mix).mp3","music/01 Everything I own by Ken Boothe 1974 Trojan TRLS95 DMk1.mp3"],
-      },
-    ],
+    playlists: initStateItem(PLAYLISTS_STATE_KEY, INITIAL_PLAYLISTS_STATE),
   }),
   actions: {
     togglePlaylistsVisible() {
@@ -32,7 +28,32 @@ export const usePlaylistStore = defineStore('playlist', {
       if (!this.playlists[this.currentPlaylist]) {
         this.currentPlaylist = this.playlists.length-1;
       }
-      this.playlists[this.currentPlaylist].tracks.push(track);
+      this.playlists = this.playlists.reduce((playlists, playlist, i) => {
+        if (i !== this.currentPlaylist) return [...playlists, playlist];
+        return [
+          ...playlists,
+          {
+            ...playlist,
+            tracks: [
+              ...playlist.tracks,
+              track,
+            ],
+          },
+        ];
+      }, []);
     },
   },
 });
+
+export const playlistSubscription = ({ events }, { playlists }) => {
+  if (events.key === 'playlists')  {
+    window.localStorage.setItem(PLAYLISTS_STATE_KEY, JSON.stringify(playlists));
+  }
+};
+
+function initStateItem(key, defaultInitiliser) {
+  const stateItem = JSON.parse(window.localStorage.getItem(key))
+  if (stateItem) return stateItem
+  return defaultInitiliser
+}
+
