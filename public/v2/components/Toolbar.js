3e02f462e952d9c6fce1e06ac2a0da806fb08d2b
seanwarman <seanwarman@gmail.com>
2024-12-30 18:10:04 +0000

    search input throttle

--- public/v2/components/Toolbar.js
+++ public/v2/components/Toolbar.js
@@ -1,6 +1,8 @@
 import { usePlayStore } from '@stores/play';
 import { usePlaylistStore } from '@stores/playlist';
 
+let timeoutId = null;
+
 export default {
   props: ['stylesheet'],
   methods: {
@@ -8,18 +10,19 @@ export default {
       usePlaylistStore().togglePlaylistMode();
     },
     onSearch({ target }) {
-      usePlayStore().onSearch(target.value);
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        usePlayStore().onSearch(target.value);
+      }, 200);
     },
     onClear() {
       usePlayStore().onSearch('');
+      this.$refs.searchInput.value = '';
     },
   },
   computed: {
     playlistMode() {
       return usePlaylistStore().playlistMode;
-    },
-    search() {
-      return usePlayStore().search;
     },
   },
   template: `
@@ -32,7 +35,7 @@ export default {
       </div>
       <div class="search-container">
         <label hidden for="search-input">Search</label>
-        <input id="search-input" type="input" placeholder="⌘ K" @input="onSearch" v-model="search" />
+        <input ref="searchInput" id="search-input" type="input" placeholder="⌘ K" @input="onSearch" />
         <button @click="onClear" tabindex="0" id="clear-search-button">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#e2e2e2" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
         </button>
